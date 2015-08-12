9cbd459ec4b61c6e3961dc5c839e090fe8b0494b
Marc-Antoine Dumont <dumont.marcantoine13@gmail.com>
2015-08-12 11:58:10 -0400

    Correction of mistake between Controller and View.

--- Assets/Controller/RessourceController.cs
+++ Assets/Controller/RessourceController.cs
@@ -1,15 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class RessourceController : MonoBehaviour {
+public class RessourceController{
+    Bank bank;
+    Text viewWood;
+    Text viewRock;
+    Text viewCorpse;
+    Text viewMeat;
 
-	// Use this for initialization
-	void Start () {
+    public RessourceController(Bank b, Text viewW, Text viewR, Text viewC, Text viewM)
+    {
+        this.bank = b;
+        this.viewCorpse = viewC;
+        this.viewWood = viewW;
+        this.viewRock = viewR;
+        this.viewMeat = viewM;
+    }
+
+    public void setRock(int nbRock){
+        bank.AmountOfRock = nbRock;
+    }
+    public int getRock(){
+        return bank.AmountOfRock;
+    }
+    public void setWood(int nbWood)
+    {
+        bank.AmountOfWood = nbWood;
+    }
+    public int getWood()
+    {
+        return bank.AmountOfWood;
+    }
+    public void setCorpse(int nbCorpse)
+    {
+        bank.AmountOfCorpse = nbCorpse;
+    }
+    public int getCorpse()
+    {
+        return bank.AmountOfCorpse;
+    }
+    public void setMeat(int nbMeat)
+    {
+        bank.AmountOfMeat = nbMeat;
+    }
+    public int getMeat()
+    {
+        return bank.AmountOfMeat;
+    }
+
+    public void UpdateView()
+    {
+        viewRock.text = ""+ getRock() +"";
+        viewMeat.text = "" + getMeat() + "";
+        viewWood.text = "" + getWood() + "";
+        viewCorpse.text = "" + getCorpse() + "";
+    }
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
