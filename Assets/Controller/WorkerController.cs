dc07b3205778bc1991290be0040ada0a5872fdad
Marc-Antoine Dumont <dumont.marcantoine13@gmail.com>
2015-08-12 18:13:42 -0400

    Modification of the name of a class.

---
+++ Assets/Controller/WorkerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WorkerController : MonoBehaviour{
+
+    public Text nbWorker;
+    public Text zombieAvailable;
+    public Text nbRessourceExpected;
+    private Stats stats;
+
+    public void AddWorker()
+    {
+        stats.AmountOfZombiesAvail --;
+        UpdateView();
+    }
+
+    public void LessWorker()
+    {
+        stats.AmountOfZombiesAvail ++;
+        UpdateView();
+    }
+
+    public void UpdateView()
+    {
+        zombieAvailable.text = stats.AmountOfZombiesAvail + "/" + stats.AmountOfZombies;
+        
+    }
+
+	// Use this for initialization
+	void Start () {
+        Stats stats = GameObject.Find("Stats").gameObject.GetComponent<Stats>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
