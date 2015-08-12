0229887a25eb56bf1552d4eff0ab9388c62dbd2a
Mike <turcotte.mike@gmail.com>
2015-08-12 11:53:25 -0400

    Committing the Maison and MaisonZombie files to the modèle folder.

---
+++ Assets/Modèle/Maison.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Maison
+{
+    private Vector2 position;
+    protected readonly int MAX_CARACTERES = 10;
+
+
+    public Maison()
+    {
+        position = new Vector2(0, 0);
+    }
+
+
+    Maison(Vector2 position)
+    {
+        this.position = position;
+    }
+
+    Vector2 getPosition()
+    {
+        return position;
+    }
+
+    void Start()
+    {
+
+    }
+
+    void update()
+    {
+
+    }
+
+}
+
---
+++ Assets/Modèle/MaisonZombie.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaisonZombie : Maison{
+
+
+    private int nbZombie;
+
+    void addZombie(int montant)
+    {
+        if (nbZombie < MAX_CARACTERES)
+        {
+            //TODO: assign zombie to this house or decrement houseless zombie
+            nbZombie++;
+        }
+
+        else
+        {
+            //TODO: alert to player: Too many zombies in one house! You must construct additional zombie houses.
+        }
+    }
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
