f1d0093b5df0b7f117261dd202cd0c5361de7b2f
Marc-Antoine Dumont <dumont.marcantoine13@gmail.com>
2015-08-13 09:36:02 -0400

    Add panel worker and modification for it.

--- Assets/Controller/WorkerController.cs
+++ Assets/Controller/WorkerController.cs
@@ -7,29 +7,53 @@ public class WorkerController : MonoBehaviour{
     public Text nbWorker;
     public Text zombieAvailable;
     public Text nbRessourceExpected;
+    public Button AddButton;
+    public Button RemoveButton;
     private Stats stats;
 
     public void AddWorker()
     {
-        stats.AmountOfZombiesAvail --;
+        stats.removeZombieAvail();
+        stats.NbZombieAssigneWood++;
         UpdateView();
     }
 
     public void LessWorker()
     {
-        stats.AmountOfZombiesAvail ++;
+        stats.addZombieAvail();
+        stats.NbZombieAssigneWood--;
         UpdateView();
     }
 
-    public void UpdateView()
+    private void UpdateView()
     {
         zombieAvailable.text = stats.AmountOfZombiesAvail + "/" + stats.AmountOfZombies;
-        
+        nbRessourceExpected.text = ""+stats.NbOfWoodByZombie * stats.NbZombieAssigneWood;
+        nbWorker.text = ""+stats.NbZombieAssigneWood;
+
+        if (stats.AmountOfZombiesAvail <= 0)
+        {
+            AddButton.enabled = false;
+        }
+        else
+        {
+            AddButton.enabled = true;
+        }
+
+        if (stats.NbZombieAssigneWood <= 0)
+        {
+            RemoveButton.enabled = false;
+        }
+        else
+        {
+            RemoveButton.enabled = true;
+        }
     }
 
 	// Use this for initialization
 	void Start () {
-        Stats stats = GameObject.Find("Stats").gameObject.GetComponent<Stats>();
+        stats = GameObject.Find("Stats").gameObject.GetComponent<Stats>();
+        UpdateView();
 	}
 	
 	// Update is called once per frame
