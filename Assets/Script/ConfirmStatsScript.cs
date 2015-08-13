1e59b9fb12e1514e70867af4702e641774460245
Michael Turcotte <turcotte.mike@gmail.com>
2015-08-13 13:59:26 -0400

    Added the panel for ending the turn that notifies the changes you're about to make to your game (still needs some functionality like cancel). Added some functions in Stats.

---
+++ Assets/Script/ConfirmStatsScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ConfirmStatsScript : MonoBehaviour {
+
+    public Text currentValues;
+    public Text addedValues;
+    public Text alertTitle;
+
+	// Use this for initialization
+	void Start () {
+        Stats stats = GameObject.Find("Stats").gameObject.GetComponent<Stats>();
+        currentValues.text = stats.displayCurrentStats();
+        addedValues.text = stats.displayAddingStats();
+        alertTitle.text = "DAY " + stats.NbTurns;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
